8f62768436562c626b4e08fda7dfd528d5b1b58c
AndreyBelyaev2026 <a.v.belyaev@hotmail.com>
2026-03-23 20:57:34 +0300

    Add func dataReading

--- main.go
+++ main.go
@@ -2,7 +2,22 @@ package main
 
 import "fmt"
 
+func dataReading() (float64, string, string) {
+	var currency1, currency2 string
+	var number float64
+	fmt.Printf("Введите название исходной валюты (USD,EUR,RUB)= ")
+	fmt.Scan(&currency1)
+	fmt.Printf("Введите название конечной валюты (USD,EUR,RUB)= ")
+	fmt.Scan(&currency2)
+	fmt.Printf("Введите количество для конвертации число с точкой (Например: 10.0)= ")
+	fmt.Scan(&number)
+	return number, currency1, currency2
+}
+
 func main() {
+	number, currency1, currency2 := dataReading()
+	fmt.Printf("%v в %v = %.2f \n", currency1, currency2, number)
+
 	const usdEur float64 = 0.87
 	const usdRub float64 = 83.87
 	//EUR в RUB
