ebc8c20c5eb43ddb5114942db47dbcd021330e48
AndreyBelyaev2026 <a.v.belyaev@hotmail.com>
2026-03-25 19:47:35 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,52 +1,81 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
 const usdEur float64 = 0.87
 const usdRub float64 = 83.87
 
+func contains(slice []string, item string) bool {
+	for _, v := range slice {
+		if v == item {
+			return true
+		}
+	}
+	return false
+}
+
 // EUR в RUB
 const rubEur float64 = usdRub / usdEur
+
+func check(number float64, currency1 string, currency2 string) bool {
+	currency := []string{"USD", "EUR", "RUB"}
+	if contains(currency, currency1) && contains(currency, currency2) && number > 0 {
+		return true
+	} else {
+		return false
+	}
+}
 
 func dataReading() (float64, string, string) {
 	var currency1, currency2 string
 	var number float64
-	fmt.Printf("Введите название исходной валюты (USD,EUR,RUB)= ")
-	fmt.Scan(&currency1)
-	fmt.Printf("Введите название конечной валюты (USD,EUR,RUB)= ")
-	fmt.Scan(&currency2)
-	fmt.Printf("Введите количество для конвертации число с точкой (Например: 10.0)= ")
-	fmt.Scan(&number)
-	return number, currency1, currency2
+
+	for {
+		fmt.Printf("Введите название исходной валюты (USD,EUR,RUB)= ")
+		fmt.Scan(&currency1)
+		fmt.Printf("Введите название конечной валюты (USD,EUR,RUB)= ")
+		fmt.Scan(&currency2)
+		fmt.Printf("Введите количество для конвертации (Например: 10.0)= ")
+		fmt.Scan(&number)
+		if check(number, currency1, currency2) {
+			return number, currency1, currency2
+		} else {
+			fmt.Println("Вы ввели неправильные данные, попробуйте ещё раз.")
+		}
+	}
 }
 
-func сurrencyСonversion(number float64, currency1 string, currency2 string) (float64, error) {
+func сurrencyСonversion(number float64, currency1 string, currency2 string) float64 {
 	var result float64
-	var err error
 
 	if currency1 == "USD" && currency2 == "RUB" {
 		result = number * usdRub
+		return result
+	} else if currency1 == "USD" && currency2 == "EUR" {
+		result = number * usdEur
+		return result
+	} else if currency1 == "EUR" && currency2 == "RUB" {
+		result = number * rubEur
+		return result
+	} else if currency1 == "EUR" && currency2 == "USD" {
+		result = number / usdEur
+		return result
+	} else if currency1 == "RUB" && currency2 == "USD" {
+		result = number / usdRub
+		return result
 	} else {
-		err = errors.New("Вы ввели неправильные данные, попробуйте ещё раз.")
+		result = number / rubEur
+		return result
 	}
 
-	return result, err
 }
 
 func main() {
-	// Получение данных
 	number, currency1, currency2 := dataReading()
-	fmt.Printf("%v в %v = %.2f \n", currency1, currency2, number)
 
-	//Example
-	result, err := сurrencyСonversion(number, currency1, currency2)
+	result := сurrencyСonversion(number, currency1, currency2)
 
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("Из %v в %v = %.2f", currency1, currency2, result)
-	}
+	fmt.Printf("%.2F %v = %.2f %v", number, currency1, result, currency2)
 }
